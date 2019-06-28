e249e576ccfcace9c11e00312c2c08d4c36505d6
geekskick <geeks_kick@yahoo.co.uk>
2019-06-28 09:20:29 +0100

    Make a debug target instead of debug.sh

--- debug.sh
+++ debug.sh
@@ -1,3 +1,5 @@
 # this file is used to create a debuggable executable, not for debug itself
 # you run this, and use it with gdb
-g++ -g -I ./Source/ -I ./Source/Console -I ./Source/Console/Log -I ./Source/Compiler -I ./Source/Compiler/Analyser ./Source/*.cpp ./Source/Console/*.cpp ./Source/Console/Log/*.cpp ./Source/Compiler/*.cpp ./Source/Compiler/Analyser/*.cpp -std=c++11 -o exec.out
+cd Tools/Build
+make setup
+make FLAGS=-g all
