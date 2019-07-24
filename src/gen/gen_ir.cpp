b0b705c4a4fe3e57f1f4c2cbf14010757e25b60c
Maxence <maxencek7@gmail.com>
2019-07-24 17:37:27 -0400

    variable declaration intermediate code

--- src/gen/gen_ir.cpp
+++ src/gen/gen_ir.cpp
@@ -6,13 +6,30 @@ assembly target code.
 */
 #include "gen_ir.h"
 
-#include <iostream>
+#include "../cli/log.h"
 
 namespace gen
 {
     std::vector<ir::operation> make_ir(tree* ast)
     {
+        cli::log(cli::log_level::debug,"--===-- IR GENERATION --===--");
         std::vector<ir::operation> ir;
+        std::vector<node*> path;
+
+        path = ast->traverse();
+
+        for(auto n : path)
+        {
+            if(n->value == "declaration")
+            {
+                auto& a_type = n->getChild(0);
+                auto& a_name = n->getChild(1);
+
+                ir::operation dec(ir::op::declaration);
+                dec.set("type",a_type.value);
+                dec.set("name",a_name.value);
+            }
+        }
 
         return ir;
     }
