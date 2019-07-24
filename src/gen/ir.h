753f4d57bcad9cae9dff19e0c7c64f8a9d50ca1b
Maxence <maxencek7@gmail.com>
2019-07-24 17:01:25 -0400

    Revert "Intermediate code operations"
    This reverts commit 181eab9414a644a1ff254976bba39f5ae9d207b2.

--- src/gen/ir.h
+++ src/gen/ir.h
@@ -1,18 +1,6 @@
 /*
 Intermediate code
 This declare the intermediate code, but doesn't generate it. This is like the relation between the parser and syntax files.
-
-The intermediate code doesn't exist as a string, but just as a vector of operation classes.
 */
-#pragma once
 
-#include <vector>
 #include <string>
-
-enum class op{declaration};
-
-class operation
-{
-    op type;
-    std::vector<std::string> args;
-};
